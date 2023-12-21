856c5715d5a1faab0d61954cd5f45c4d42aa9eed
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-21 13:10:31 +0100

    chore: more update

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -4,6 +4,34 @@ import style from './HomePage.module.scss';
 
 export function HomePage(): JSX.Element {
     return (
-        <div className={ style.container }>HOME PAGE</div>
+        <div className={ style.container }>
+            <nav></nav>
+            <body>
+                <header></header>
+                <section>
+                    body1
+                </section>
+
+                <section>
+                    body2
+                </section>
+
+                <section>
+                    body3
+                </section>
+
+                <section>
+                    body4
+                </section>
+
+                <section>
+                    body5
+                </section>
+
+                <footer>
+                    
+                </footer>
+            </body>
+        </div>
     );
 }
