65eb8f7e6c3e983a96b0f57685a3c51b2838c520
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-22 18:21:08 +0100

    chore: $1

--- src/components/Body3/Body3.tsx
+++ src/components/Body3/Body3.tsx
@@ -1,6 +1,40 @@
 import { JSX } from 'react';
 
 import style from './Body3.module.scss';
+// import { title } from 'process';
+
+const card = [
+    {
+        logo: 'https://assets-global.website-files.com/640bf70a17d12b42d97a052b/643ecd39cf86b17d54462fb9_secure2x.svg',
+        title: 'Price competitiveness',
+        special: 'https://assets-global.website-files.com/640bf70a17d12b42d97a052b/640ed40a53155cfb1b658a08_price-competivness-icons-2x.png',
+        description: 'A single API integration unlocks thousands of pairs with unrivaled liquidity from 100+ exchanges and professional market makers.',
+    },
+
+    {
+        logo: 'https://assets-global.website-files.com/640bf70a17d12b42d97a052b/643ecd39e79d3992faae6170_data2x.svg',
+        title: 'Enriched Data APIs to help you build',
+        description: 'Access the most comprehensive and curated token metadata to help you expand your product.',
+    },
+
+    {
+        logo: 'https://assets-global.website-files.com/640bf70a17d12b42d97a052b/643ecd395c3f535fe702ec3c_dashimage2x.svg',
+        title: '0x Dashboard',
+        description: 'Generate API keys, monitor and manage your integrations, get access to new APIs and tap into more developer resources.',
+    },
+
+    {
+        logo: 'https://assets-global.website-files.com/640bf70a17d12b42d97a052b/643ecd39cf86b17d54462fb9_secure2x.svg',
+        title: 'Secure and audited smart contracts',
+        description: 'Security is critical for our developers and their users. Third party audits ensure that there are no vulnerabilities to exploit.',
+    },
+
+    {
+        logo: 'https://assets-global.website-files.com/640bf70a17d12b42d97a052b/643ecd398585c5aa4ed8c896_enterprise2x.svg',
+        title: 'Enterprise-grade DEX infrastructure',
+        description: 'Designed to meet the requirements of Web3 apps, exchanges and financial institutions with 99.9% uptime.',
+    },
+]
 
 export function Body3(): JSX.Element {
     return (
@@ -20,11 +54,40 @@ export function Body3(): JSX.Element {
 
                 <div className={ style.grid }>
                     <div className={ style.grid_container }>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                        {
+                            card.map((el, i) => {
+                                if (el.special) {
+                                    return (
+                                        <div
+                                            key={i}
+                                        >
+                                            <div className="left">
+                                                <div className="left_container">
+                                                    <div className="logo"></div>
+                                                    <div className="title"></div>
+                                                    <div className="description"></div>
+                                                </div>
+                                            </div>
+                                            <div className="right">
+                                                <img src="." alt="mmagie" />
+                                            </div>
+                                        </div>
+                                    )
+                                }
+
+                                return (
+                                    <div
+                                        key={i}
+                                    >
+                                        <div className="card">
+                                            <div className="logo"></div>
+                                            <div className="title"></div>
+                                            <div className="description"></div>
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        }
                     </div>
                 </div>
             </div>
