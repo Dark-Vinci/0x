6f09dbe19a36bd8ade4e4107a20d1dcff8eb8e99
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-22 01:55:46 +0100

    chore: more commits

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,73 @@
 import { JSX } from 'react';
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaReddit, FaGithub, FaArrowRight } from "react-icons/fa";
 
 import style from './Footer.module.scss';
 
 export function Footer(): JSX.Element {
     return (
         <div className={ style.container }>
-            Footer
+            <div className={ style.footer_container }>
+                <div className={ style.top }>
+                    <div className={[style.footer_card, style.f].join(' ')}>
+                        <div className="top_container">
+                            <div className="title">
+                                <p>Ready to start building?</p>
+                            </div>
+                            <div className="description">
+                                <p>Create a 0x account and get started, for free, in less than five minutes.</p>
+                            </div>
+                            <div className="button">
+                                <button>Create Account</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={[style.footer_card, style.s].join(' ')}>
+                        <div className={style.bot_container }>
+                            <div className={style.title }>
+                                <p>Join our newsletter</p>
+                            </div>
+                            <div className={style.description }>
+                                <p>Stay in the know on the latest alpha, news and product updates.</p>
+                            </div>
+
+                            <div className={style.input }>
+                                <div className={ style.input_container }>
+                                    <input 
+                                        type="text" 
+                                        placeholder='Enter your email'
+                                    />
+                                    <div className={style.icon }>
+                                        <FaArrowRight color='#fff' size='20px'/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className={style.terms }>
+                                <p>To learn how we manage your data, visit our</p>
+                                <a href=".">Privacy Policy</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={ style.bottom }>
+                    <div className={ style.main }>Main</div>
+                    <div className={ style.sub }>
+                        <div className={ style.sub_container }>
+                            <div className={ style.left }>
+                                <p>© ZeroEx Holdings, Inc. All rights reserved.</p>
+                            </div>
+                            <div className={ style.right }>
+                                <AiFillTwitterCircle />
+                                <FaReddit />
+                                <FaGithub />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,7 +5,31 @@ import style from './Nav.module.scss';
 export function Nav(): JSX.Element {
     return (
         <div className={ style.container }>
-            NAVIGATION
+            <div className ={ style.nav_container }>
+                <div className={ style.left }>
+                    <img 
+                        src="https://assets-global.website-files.com/640bf70a17d12b42d97a052b/640bfd7d8441821c4cd20210_logo.svg" 
+                        alt="logo" 
+                    />
+                </div>
+
+                <div className={ style.mid }>
+                    <ul>
+                        <li><a href=".">Products</a></li>
+                        <li><a href=".">Solutions</a></li>
+                        <li><a href=".">Pricing</a></li>
+                        <li><a href=".">Docs</a></li>
+                        <li><a href=".">Content Hub</a></li>
+                    </ul>
+                </div>
+
+                <div className={ style.right }>
+                    <ul>
+                        <li><a href=".">Contact Us</a></li>
+                        <li className={style.invert}><a href=".">Go to dashboard</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
