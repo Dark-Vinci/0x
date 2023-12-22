9eb553fe2fcaaaf8880588d8b002377e289aa534
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-22 04:23:38 +0100

    chore: more update on elements

--- src/components/Body1/Body1.tsx
+++ src/components/Body1/Body1.tsx
@@ -5,7 +5,38 @@ import style from './Body1.module.scss';
 export function Body1(): JSX.Element {
     return (
         <div className={ style.container }>
-            BODY1
+            <div className={ style.body1_container }>
+                <div className={ style.company }>
+                    <div className={ style.company_container }>
+                        <p>Trusted by leading Web3 apps</p>
+                        <div className={ style.image_grid }></div>
+                    </div>
+                </div>
+
+                <div className={ style.title }>
+                    <p>A fully integrated suite of <span>APIs</span></p>
+                </div>
+                <div className={ style.grid }>
+                    <div className={style.fade}></div>
+                    <div>
+                        <a href=".">
+                            <Card 
+                                imageUrl={''} 
+                                title={''} 
+                                description={''} 
+                                isComingSoon={false} 
+                            />
+                        </a>
+                    </div>
+                    <div className={style.fade}></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div className={style.fade}></div>
+                    <div></div>
+                </div>
+            </div>
         </div>
     );
 }
